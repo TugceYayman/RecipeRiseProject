d69efc8c56a0e323879151a78274f3f796b4533c
Tugce Yayman <tugce123yay@gmail.com>
2024-04-19 21:12:31 +0100

    Fix

--- recipeRise/myProject/myProject/settings.py
+++ recipeRise/myProject/myProject/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
