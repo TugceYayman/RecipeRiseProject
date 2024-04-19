b2fb86356be2fc169fb6d997b8af611e11210c40
Tugce Yayman <tugce123yay@gmail.com>
2024-04-19 16:43:42 +0100

    Sing-up dialog boxes

--- recipeRise/src/app/signup/signup.component.ts
+++ recipeRise/src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { UpdateDialogComponent } from '../update-dialog/update-dialog.component';
 
 @Component({
   selector: 'app-signup',
@@ -14,36 +16,48 @@ export class SignupComponent {
   confirmPassword: string = '';
 
   constructor(private authService: AuthService,
-    private router: Router) { }
+    private router: Router, private dialog: MatDialog) { }
 
   onSignup(): void {
     if (this.password === this.confirmPassword) {
       this.authService.signup(this.username, this.email, this.password).subscribe(
         (response: any) => {
-          alert(response.message);
+         // alert(response.message);
+          this.dialog.open(UpdateDialogComponent, {
+            data: { title: 'Success', message: 'Your account has been successfully created!' },
+          });
           this.router.navigate(['/login']);
         },
         (error: any) => {
           if (error.status === 400) {
             // Handle username error
             if (error.error && error.error.username && error.error.username[0].includes('custom user with this username already exists.')) {
-              alert("The username is already taken. Please choose a different one.");
+              this.dialog.open(UpdateDialogComponent, {
+                data: { title: 'Fail', message: 'The username is already taken. Please choose a different one.' },
+              });
             }
             // Handle email error similarly if backend provides a specific message for it
             else if (error.error && error.error.email && error.error.email[0].includes('custom user with this email address already exists.')) {
-              alert("The email is already taken. Please choose a different one.");
+              this.dialog.open(UpdateDialogComponent, {
+                data: { title: 'Fail', message: 'The email is already taken. Please choose a different one.' },
+              });
             }
             else {
-              alert("There was a problem with the data you entered. Please check and try again.");
+              this.dialog.open(UpdateDialogComponent, {
+                data: { title: 'Fail', message: 'There was a problem with the data you entered. Please check and try again.' },
+              });
             }
           } else {
-            alert("An unexpected error occurred. Please try again later.");
+            this.dialog.open(UpdateDialogComponent, {
+              data: { title: 'Fail', message: 'An unexpected error occurred. Please try again later.' },
+            });
           }
-          console.error('Error during signup', error);
         }
       );
     } else {
-      alert("Passwords do not match.");
+      this.dialog.open(UpdateDialogComponent, {
+        data: { title: 'Fail', message: 'Passwords do not match.' },
+      });
     }
   }
 }    
